b5020f230b54d474a69eff2682355c2fb028485f
joshuastewart417 <joshua.stewart780@gmail.com>
2021-08-27 11:13:17 -0500

    updated styling for entry list

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -6,10 +6,10 @@ export const JournalEntryComponent = (entry) => {
   return `
         <section id="entry--${entry.id}" class="journalEntry">
             <ul>
-					<li class="entry-details">Date: ${entry.date}</li>
-					<li class="entry-details">Mood: ${entry.mood}</li>
-					<li class="entry-details">Concept: ${entry.concept} inches</li>
-					<li class="entry-details">Entry: ${entry.entry}</li>
+					<li class="entry-details"><strong>Date:</strong> ${entry.date}</li>
+					<li class="entry-details"><strong>Mood:</strong> ${entry.mood}</li>
+					<li class="entry-details"><strong>Concept:</strong> ${entry.concept} inches</li>
+					<li class="entry-details"><strong>Entry:</strong> ${entry.entry}</li>
 			</ul>
         </section>
     `;
